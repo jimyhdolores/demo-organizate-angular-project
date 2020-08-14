d51a4c914e961dd574d46e7f206ef3c7aadfa5c4
Jimy Huacho Dolores <jimmyhd_19@hotmail.com>
2020-08-14 18:33:01 -0500

    configuración inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
--- src/app/common/constants/url-http.ts
+++
@@ -1,4 +0,0 @@
-import { environment } from '../../../environments/environment';
-
-export const PATCH = environment.path_mock;
-export const POKEMON_LIST = PATCH + 'pokemons';
